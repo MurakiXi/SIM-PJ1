fe7053709f32e5032af7c543b5ad9aeda2f0efe2
sageien <l35makita@hotmail.co.jp>
2026-01-09 15:21:21 +0900

    配送先住所編集

--- src/app/Http/Requests/AddressRequest.php
+++ src/app/Http/Requests/AddressRequest.php
@@ -13,7 +13,7 @@ class AddressRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return auth()->check();
     }
 
     /**
@@ -24,9 +24,9 @@ class AddressRequest extends FormRequest
     public function rules()
     {
         return [
-            'postal_code' => ['required', 'regex:/^\d{3}-\d{4}$/'],
-            'address'     => ['required', 'string'],
-            'building'    => ['nullable', 'string'],
+            'postal_code' => ['required', 'string', 'regex:/^\d{3}-\d{4}$/'],
+            'address'     => ['required', 'string', 'max:255'],
+            'building'    => ['nullable', 'string', 'max:255'],
         ];
     }
 }
---
+++ src/resources/views/purchase/address.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+
+@section('title', '送付先住所変更')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/purchase.css') }}">
+@endsection
+
+@section('content')
+
+<div class="edit-address__header">
+    <h1>住所の変更</h1>
+</div>
+
+<form class="edit-address__form" action="{{ route('purchase.address.update', $item) }}" method="POST">
+    @csrf
+    @method('PATCH')
+
+    <div class="edit-address__form-item">
+        <label>郵便番号</label>
+        <input type="text" class="edit-address__item-input" name="postal_code" value="{{ old('postal_code', optional($address)->postal_code) }}">
+        @error('postal_code') <p class="form__error">{{ $message }}</p> @enderror
+    </div>
+    <div class="edit-address__form-item">
+        <label>住所</label>
+        <input type="text" class="edit-address__item-input" name="address" value="{{old('address',optional($address)->address)}}">
+        @error('address') <p class="form__error">{{ $message }}</p> @enderror
+    </div>
+    <div class="edit-address__form-item">
+        <label>建物名</label>
+        <input type="text" class="edit-address__item-input" name="building" value="{{old('building',optional($address)->building)}}">
+        @error('building') <p class="form__error">{{ $message }}</p> @enderror
+    </div>
+    <div class="edit-address__button">
+        <button class="edit-address__button-submit" type="submit">更新する</button>
+    </div>
+</form>
+
+@endsection
